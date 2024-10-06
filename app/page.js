9a2c308e367d3bca25cbce2c0b59bcf8c5142b65
FeRN <fasakhor@gmail.com>
2024-10-06 22:08:55 +0600

    Finished with the prototype

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import Postcard from "@/component/Postcard"
+import getPostMetadata from "@/utils/getPostMetadata";
 
 
 export default function Home() {
+  const postMetadata= getPostMetadata('recipes')
   return (
     <main>
-
+        <div clasName='postContainer'>
+            {postMetadata.map((post, postIndex)=>{
+                return(
+                    <Postcard key={postIndex} post={post}/>
+                )
+            })}
+        </div>
     </main>
   );
 }
